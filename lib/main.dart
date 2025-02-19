770532a7a0cd15645982da86374441c2249b0bba
aaonykj21 <aon.natcha29@gmail.com>
2025-02-19 14:46:41 +0700

    homework 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 // import 'package:demoproject/api/simple_api.dart';
-import 'package:demoproject/Homework2/air_quality.dart';
+// import 'package:demoproject/Homework2/air_quality.dart';
+// import 'package:demoproject/fire_store/fire_store_demo.dart';
+import 'package:demoproject/product/product_api.dart';
 // import 'package:demoproject/form_widget/form_simple.dart';
 // import 'package:demoproject/page_answer/answer1.dart';
 // import 'package:demoproject/my_statefull_widget.dart';
@@ -8,18 +10,19 @@ import 'package:demoproject/Homework2/air_quality.dart';
 // import 'package:demoproject/aqi/air_quality.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: AirQuality());
+    return const MaterialApp(home: Product());
   }
 }
-
